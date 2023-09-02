69e56a1969e14b8b7beaf4b99d5aaf31ee393df6
Greetly-ch <141371976+Greetly-ch@users.noreply.github.com>
2023-09-02 16:38:11 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 
-
+# Apply database migrations
+echo "Apply database migrations"
 python manage.py migrate
 
-exec python manage.py runserver 0.0.0.0:8000
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
