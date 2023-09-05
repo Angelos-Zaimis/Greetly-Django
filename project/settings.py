7b641540c62577cdde3f295b81c315cfffd3d669
Greetly-ch <141371976+Greetly-ch@users.noreply.github.com>
2023-09-05 20:00:07 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -104,25 +104,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('QOVERY_POSTGRESQL_ZF9F2352B_DEFAULT_DATABASE_NAME','postgres'),
         'USER': os.environ.get('QOVERY_POSTGRESQL_ZF9F2352B_LOGIN','postgres'),
         'PASSWORD': os.environ.get('QOVERY_POSTGRESQL_ZF9F2352B_PASSWORD'),
-        'PORT': os.environ.get('QOVERY_POSTGRESQL_ZF9F2352B_PORT',5432),
         'HOST': os.environ.get('QOVERY_POSTGRESQL_ZF9F2352B_HOST','zf9f2352b-postgresql.z503fe5f4.scsi.sh'),
+        'PORT': os.environ.get('QOVERY_POSTGRESQL_ZF9F2352B_PORT',5432)
     }
 }
 
