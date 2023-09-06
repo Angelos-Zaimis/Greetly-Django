13eccde9d010935de91c089352b4b0cceccd8315
Greetly-ch <141371976+Greetly-ch@users.noreply.github.com>
2023-09-06 22:53:34 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -179,6 +179,7 @@ DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
