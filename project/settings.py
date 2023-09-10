86c6a44ae91d136ee04d72b66ca60957720ec2d8
Angelos Zaimis <angelos.zaimis.dev@gmail.com>
2023-09-10 21:13:47 +0200

    deployment changes

--- project/settings.py
+++ project/settings.py
@@ -162,8 +162,6 @@ SIMPLE_JWT = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
@@ -188,7 +186,8 @@ else:
     MEDIA_URL = 'media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
