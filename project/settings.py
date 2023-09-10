933bb6333955ccdc48c831b5076482f3cb3dab9e
Angelos Zaimis <angelos.zaimis.dev@gmail.com>
2023-09-10 22:09:35 +0200

    deployment changes

--- project/settings.py
+++ project/settings.py
@@ -169,7 +169,7 @@ SIMPLE_JWT = {
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+
 
 if SERVER_TYPE == 'production':
     # production settings
