e44818f5d2e4104962b92473a1ca8d70f7f6a152
Angelos Zaimis <angelos.zaimis.dev@gmail.com>
2023-09-15 23:39:24 +0200

    image url

--- Cities/models.py
+++ Cities/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=100, default='')
     city = models.ForeignKey(City, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='category_images/', default='', blank=True)
+    image = models.ImageField(upload_to='city_images/', default='', blank=True)
     icon = models.CharField(max_length=200, default='')
 
     def __str__(self):
