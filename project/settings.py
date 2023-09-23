fe38498897322c333d2bde23b3e33bf1efd6726a
Angelos Zaimis <angelos.zaimis.dev@gmail.com>
2023-09-23 19:35:48 +0200

    db

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-
+import logging
 # settings.py
 import dj_database_url
 
@@ -118,6 +118,10 @@ DATABASES = {
 if SERVER_TYPE == 'production':
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 
+
+logger = logging.getLogger('django.db.backends')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
