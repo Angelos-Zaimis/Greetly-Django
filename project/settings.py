6d02db85ef53b7848e3c17580d400579998f1de8
Angelos Zaimis <angeloszaimis@Angeloss-MacBook-Air.local>
2024-02-20 21:36:26 +0100

    f

--- project/settings.py
+++ project/settings.py
@@ -197,7 +197,7 @@ SIMPLE_JWT = {
 # )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if SERVER_TYPE == 'production':
     # production settings
