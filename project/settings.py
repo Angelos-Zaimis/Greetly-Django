819a50e142edd5674cfc53c98dfa007f635f7b4e
Angelos Zaimis <angeloszaimis@Angeloss-MacBook-Air.local>
2024-02-22 21:53:51 +0100

    feat

--- project/settings.py
+++ project/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
