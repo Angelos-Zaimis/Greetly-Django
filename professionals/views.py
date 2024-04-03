4b73fde3e2083a47f502b60ca4d407e969f46202
Angelos Zaimis <angeloszaimis@Angeloss-MacBook-Air.local>
2024-04-03 23:10:13 +0200

    feat: professionals

--- professionals/views.py
+++ professionals/views.py
@@ -1,9 +1,10 @@
+from django.http import Http404
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-
-from professionals.models import Professionals
-from professionals.serializers import ProfessionalSerializer
+from .models import Professionals  # Adjust the import according to your app structure
+from .serializers import ProfessionalSerializer  # Adjust the import according to your app structure
 
 
 class ProfessionalListView(generics.ListAPIView):
@@ -12,12 +13,31 @@ class ProfessionalListView(generics.ListAPIView):
 
     def get_queryset(self):
         professional_type = self.request.query_params.get('type')
+        canton = self.request.query_params.get('canton')
 
-        if professional_type:
-            queryset = Professionals.objects.filter(type=professional_type)
-            if queryset.exists():
-                return queryset
-            else:
-                return Response({"message": f"Error: No professionals found with type '{professional_type}'."}, status=status.HTTP_404_NOT_FOUND)
-        else:
-            return  Response({"message": "Error: Professional type not specified."}, status=status.HTTP_404_NOT_FOUND)
+        if not professional_type or not canton:
+            # Using ValidationError to indicate bad request data
+            raise ValidationError("Professional type and canton must be specified.")
+
+        queryset = Professionals.objects.filter(type=professional_type, canton=canton)
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        try:
+            queryset = self.filter_queryset(self.get_queryset())
+        except ValidationError as e:
+            # Return a 400 Bad Request response if there is a validation error
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not queryset.exists():
+            # Raising Http404 will be caught by DRF and return a 404 Not Found response
+            raise Http404(
+                f"No professionals found with type '{request.query_params.get('type')}' in canton '{request.query_params.get('canton')}'.")
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
